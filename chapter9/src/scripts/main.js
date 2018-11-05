f4fb9725da105f3bfb532b111a45f12986e4f626
Ousama Elayan <ousamasama@gmail.com>
2018-11-04 20:05:27 -0600

    forgot to upload chapter 9

---
+++ chapter9/src/scripts/main.js
@@ -0,0 +1,15 @@
+//In main.js, import the ContactList component and the ContactForm component.
+
+//The user should see the contact form at the top of the view,
+//and the list of contacts underneath it.
+
+// import sayHello from "./hello"
+// import sayGoodbye from "./goodbye"
+// import SandwichMaker from "./sandwichMaker"
+import ContactList from "./contactList"
+import new_form from "./contactForm"
+
+// sayHello()
+// sayGoodbye()
+
+// SandwichMaker.placeOrder("rye", "capicola", "provolone")
